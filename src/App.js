3802af184b1ec519b9b09f9b23d26c93b9bc97d7
said-pc <webhost48@gmail.com>
2021-09-21 17:13:59 +0100

    first push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import {
+  TextField,
+  Box,
+  CssBaseline,
+  Button,
+  Typography,
+  Divider,
+  Alert
+} from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { fetch_data } from "./redux/actions/actions";
+import { LineChart, YAxis, XAxis, CartesianGrid, Line } from "recharts";
 
 function App() {
+  const dispatch = useDispatch();
+  const inpRef = useRef(null);
+  const fetched_data = useSelector((state) => state.ui.data);
+  //handle search btn click
+  const handleSearch = (event) => {
+    // we will get our input value to get country code -- later --
+    console.log("country code :", inpRef.current.value);
+    dispatch(
+      fetch_data({
+        country_code: inpRef.current.value != "" ? inpRef.current.value : "dz"
+      })
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <CssBaseline />
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyConten="center"
+        alignItems="center"
+        sx={{ width: "100%", pt: 2 }}
+      >
+        {/* replace with message component */}
+        {/* <Typography variant="h4">Welcome to COVID-19 TRACKER</Typography> */}
+        <Alert severity="info">Welcome to COVID-19 TRACKER</Alert>
+        <Divider flexItem orientation="horizontal" sx={{ mb: 2, mt: 1 }} />
+        <TextField
+          placeholder="search"
+          size="small"
+          inputRef={inpRef}
+          sx={{
+            width: "80%",
+            margin: "auto",
+            "& .MuiOutlinedInput-input": {
+              textAlign: "center"
+            }
+          }}
+        />
+        <Button
+          variant="outlined"
+          color="info"
+          size="small"
+          sx={{ mt: 2, width: "50%" }}
+          onClick={(event) => handleSearch(event)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Search
+        </Button>
+        <Box sx={{ mt: 2, bgcolor: "red", width: "100%" }}>
+          {fetched_data ? (
+            fetched_data.map((el, index) => <Box>{el.Deaths}</Box>)
+          ) : (
+            <></>
+          )}
+        </Box>
+      </Box>
+    </>
   );
 }
 
